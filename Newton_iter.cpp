619babdb663b5bf57fbf5af950c8942def0e5842
zry <zhengry22@mails.tsinghua.edu.cn>
2024-08-27 14:29:37 +0800

    feat: Add Newton's method for further tests if needed

---
+++ Newton_iter.cpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "Newton_iter.h"
+using namespace std;
+
+int main() {
+    
+}
--- Newton_iter.h
+++ Newton_iter.h
@@ -16,23 +16,34 @@ std::unordered_map<std::function<double(double)>, std::function<double(double)>>
     {silu, silu_second_derivative}
 };
 
+
 template<typename T, typename U> 
-T Newton_iteration(T (*calc_func)(U), Polynomial<T> &poly, T low, T high) {
+T Newton_iteration(T (*calc_func)(U), const Polynomial<T> &poly, T low, T high) {
     /* Look for 1st and 2nd derivative */
-    if (derivative_.find(calc_func) != derivative_.end()) {
-        return derivative_[calc_func];
-    } else {
+    if (derivative_.find(calc_func) == derivative_.end()) {
         throw std::invalid_argument("Derivative function not found for the given function.");
     }
 
-    if (derivative_second.find(calc_func) != derivative_second.end()) {
-        return derivative_second[calc_func];
-    } else {
+    if (derivative_second.find(calc_func) == derivative_second.end()) {
         throw std::invalid_argument("2nd Derivative function not found for the given function.");
     }
 
     T max_point = (low + high) / 2; /* This is the point we choose at first */
+    
 
-    poly.derivative()
-
+    for (int i = 0; i < NEWTON_MAX_ITERATION; i++) {
+        T derivative_1 = derivative_[calc_func](max_point);
+        T derivative_2 = derivative_second[calc_func](max_point);
+        Polynomial<T> first_der = poly.derivative();
+        Polynomial<T> second_der = poly.second_derivative();
+        T poly_result_1 = first_der.get_poly_value(max_point);
+        T poly_result_2 = second_der.get_poly_value(max_point);
+        max_point = max_point - (derivative_1 - poly_result_1) / (derivative_2 - poly_result_2);
+    }
+    
+    return max_point;
 }
+
+
+template<typename T, typename U>
+T 
