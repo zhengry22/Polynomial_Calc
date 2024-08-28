2213f147d06cc6651c3dfaac81511485b4839cbe
zry <zhengry22@mails.tsinghua.edu.cn>
2024-08-28 16:00:46 +0800

    feat: add more testing functions

--- SiLU.h
+++ SiLU.h
@@ -1,7 +1,8 @@
 #pragma once
 #include<cmath>
 #define B 1
-#define RATIO 0.1
+#define K 1.1
+#define RATIO 0.5
 #define RAN 2
 using namespace std;
 
@@ -27,6 +28,11 @@ double relu(double x) {
 
 double silu(double x) {
     return x * sigmoid(x);
+}
+
+
+double silu_with_k(double x) {
+    return K * silu(x);
 }
 
 
@@ -60,6 +66,10 @@ double circle_relu(double x) {
     }
 }
 
+double silu_with_bias(double x) {
+    if (x < 3 && x > 0.25) {return silu(x) + 0.25;}
+    return silu(x);
+}
 
 double square_plus(double x) {
     return (x + sqrt(x * x + B)) / 2;
@@ -74,6 +84,34 @@ double gelu(double x) {
     return 0.5 * x * (1.0 + std::erf(x / std::sqrt(2.0)));
 }
 
+
+double better_gelu(double x) {
+    if (abs(x) >= 1) {return gelu(x);}
+    return relu(x);
+}
+
 double gelu_and_sqplus(double x) {
     return (square_plus(x) * RATIO) + (gelu(x) * (1 - RATIO)); 
+}
+
+double gelu_with_k(double x) {
+    if (x > 0 && x < 1) {
+        return (2 - x) * gelu(x);
+    }
+    else if (x > 1 && x < 1.5) {
+        return (2.5 - x) * gelu(x);
+    }
+    else {
+        return gelu(x);
+    }
+}
+
+
+double tanh(double x) {
+    return (exp(x) - exp(-x)) / (exp(x) + exp(-x));
+}
+
+
+double mix_relu_tanh(double x) {
+    return mix_relu(x) * (tanh(4 * x - 1) * 0.5 + 0.5);
 }
